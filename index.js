8353f574b2cb164912d5a18f62bf44d7aa3e2311
Nam Truong <truongthanhnamdev@gmail.com>
2020-09-13 19:12:15 +0700

    aaa

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 const path = require("path");
+app.use(
+    expressStaticGzip(path.join(__dirname, 'build'), {
+        enableBrotli: true, // only if you have brotli files too
+    }),
+);
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
