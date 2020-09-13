6a42fe99fa670a39a539cca1d9b2edbae6b85ffd
Nam Truong <truongthanhnamdev@gmail.com>
2020-09-13 19:42:41 +0700

    aaa

--- index.js
+++ index.js
@@ -4,14 +4,7 @@ var expressStaticGzip = require("express-static-gzip");
 const app = express()
 const path = require("path");
 app.use(
-    expressStaticGzip(path.join(__dirname, 'build'), {
-        enableBrotli: true,
-        customCompressions: [{
-            encodingName: 'deflate',
-            fileExtension: 'zz'
-        }],
-        orderPreference: ['br']
-    }),
+    expressStaticGzip(path.join(__dirname, 'build'))
 );
 
 app.use(express.static(path.join(__dirname, 'build')));
