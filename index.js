6feae4005007952bb815584aba5430eec1d43dd2
Nam Truong Thanh <nam.truongthanh@gameloft.com>
2020-09-28 11:17:51 +0700

    #123

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(process.env.PORT || 5000, () => {
-    console.log('App listening on port 5000')
+    console.log('App listening on port 50001')
 })
